d060fb32fc977fa6e0bc6d223b0b4c16bc9af8fb
panne <unknown>
2002-05-25 15:31:38 +0000

    [project @ 2002-05-25 15:31:38 by panne] Hackily add GLenum

--- Graphics/Rendering/OpenGL/GL/BasicTypes.hs
+++ Graphics/Rendering/OpenGL/GL/BasicTypes.hs
@@ -1,7 +1,9 @@
 module Graphics.Rendering.OpenGL.GL.BasicTypes where
 
 import Data.Int ( Int32 )
+import Data.Word ( Word32 )
 
 type GLint    = Int32
+type GLenum   = Word32
 type GLfloat  = Float
 type GLdouble = Double
