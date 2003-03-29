93390eb4db1aaed1b07d94a073549033b1079c30
panne <unknown>
2003-03-29 17:39:05 +0000

    [project @ 2003-03-29 17:39:05 by panne] Added license notice

--- Graphics/Rendering/OpenGL.hs
+++ Graphics/Rendering/OpenGL.hs
@@ -1,4 +1,4 @@
------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 -- |
 -- Module      :  Graphics.Rendering.OpenGL
 -- Copyright   :  (c) Sven Panne 2002
@@ -8,16 +8,45 @@
 -- Stability   :  experimental
 -- Portability :  portable
 --
--- A convenience module which combines the Haskell bindings for OpenGL, the
--- industry\'s most widely used and supported 2D and 3D graphics API, and
--- it\'s accompanying utility library.
+-- A convenience module, combining the Haskell bindings for GL and GLU.
 --
------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 
 module Graphics.Rendering.OpenGL (
+   -- * Legal stuff
+
+   -- $LegalStuff
+
      module Graphics.Rendering.OpenGL.GL
    , module Graphics.Rendering.OpenGL.GLU
 ) where
 
 import Graphics.Rendering.OpenGL.GL
 import Graphics.Rendering.OpenGL.GLU
+
+--------------------------------------------------------------------------------
+-- $LegalStuff
+-- The documentation is loosely based on the man pages of the OpenGL Sample
+-- Implemenation from SGI, see: <http://oss.sgi.com/projects/ogl-sample/>. It is
+-- used under the SGI Free Software License B. This license requires the
+-- following notice:
+--
+-- /License Applicability/. Except to the extent portions of this file are made
+-- subject to an alternative license as permitted in the SGI Free Software
+-- License B, Version 1.1 (the \"License\"), the contents of this file are
+-- subject only to the provisions of the License. You may not use this file
+-- except in compliance with the License. You may obtain a copy of the License
+-- at Silicon Graphics, Inc., attn: Legal Services, 1600 Amphitheatre Parkway,
+-- Mountain View, CA 94043-1351, or at: <http://oss.sgi.com/projects/FreeB/>.
+-- 
+-- Note that, as provided in the License, the Software is distributed on an \"AS
+-- IS\" basis, with ALL EXPRESS AND IMPLIED WARRANTIES AND CONDITIONS
+-- DISCLAIMED, INCLUDING, WITHOUT LIMITATION, ANY IMPLIED WARRANTIES AND
+-- CONDITIONS OF MERCHANTABILITY, SATISFACTORY QUALITY, FITNESS FOR A PARTICULAR
+-- PURPOSE, AND NON-INFRINGEMENT.
+--
+-- /Original Code/. The Original Code is: OpenGL Sample Implementation, Version
+-- 1.2.1, released January 26, 2000, developed by Silicon Graphics, Inc. The
+-- Original Code is Copyright (c) 1991-2002 Silicon Graphics, Inc.  Copyright in
+-- any portions created by third parties is as indicated elsewhere herein. All
+-- Rights Reserved.
