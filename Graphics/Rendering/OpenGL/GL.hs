3614b643f6c28d2921e12e86c13719da24cfed49
panne <unknown>
2003-06-26 17:11:47 +0000

    [project @ 2003-06-26 17:11:47 by panne] Improve documentation a bit

--- Graphics/Rendering/OpenGL/GL.hs
+++ Graphics/Rendering/OpenGL/GL.hs
@@ -14,51 +14,67 @@
 -----------------------------------------------------------------------------
 
 module Graphics.Rendering.OpenGL.GL (
-     module Graphics.Rendering.OpenGL.GL.Antialiasing,
-     module Graphics.Rendering.OpenGL.GL.BasicTypes,
-     module Graphics.Rendering.OpenGL.GL.BeginEnd,
-     module Graphics.Rendering.OpenGL.GL.Clipping,
-     module Graphics.Rendering.OpenGL.GL.Colors,
-     module Graphics.Rendering.OpenGL.GL.CoordTrans,
-     module Graphics.Rendering.OpenGL.GL.FlushFinish,
-     module Graphics.Rendering.OpenGL.GL.Fog,
-     module Graphics.Rendering.OpenGL.GL.Framebuffer,
-     module Graphics.Rendering.OpenGL.GL.Hints,
-     module Graphics.Rendering.OpenGL.GL.LineSegments,
-     module Graphics.Rendering.OpenGL.GL.PerFragment,
-     module Graphics.Rendering.OpenGL.GL.PixelRect,
-     module Graphics.Rendering.OpenGL.GL.PixelRectangles,
-     module Graphics.Rendering.OpenGL.GL.Points,
-     module Graphics.Rendering.OpenGL.GL.Polygons,
-     module Graphics.Rendering.OpenGL.GL.RasterPos,
-     module Graphics.Rendering.OpenGL.GL.Rectangles,
-     module Graphics.Rendering.OpenGL.GL.StateVar,
-     module Graphics.Rendering.OpenGL.GL.StringQueries,
-     module Graphics.Rendering.OpenGL.GL.Texturing,
-     module Graphics.Rendering.OpenGL.GL.VertexArrays,
-     module Graphics.Rendering.OpenGL.GL.VertexSpec
+   -- * OpenGL Operation
+   module Graphics.Rendering.OpenGL.GL.BasicTypes,
+   module Graphics.Rendering.OpenGL.GL.BeginEnd,
+   module Graphics.Rendering.OpenGL.GL.VertexSpec,
+   module Graphics.Rendering.OpenGL.GL.VertexArrays,
+   module Graphics.Rendering.OpenGL.GL.Rectangles,
+   module Graphics.Rendering.OpenGL.GL.CoordTrans,
+   module Graphics.Rendering.OpenGL.GL.Clipping,
+   module Graphics.Rendering.OpenGL.GL.RasterPos,
+   module Graphics.Rendering.OpenGL.GL.Colors,
+
+   -- * Rasterization
+   module Graphics.Rendering.OpenGL.GL.Antialiasing,
+   module Graphics.Rendering.OpenGL.GL.Points,
+   module Graphics.Rendering.OpenGL.GL.LineSegments,
+   module Graphics.Rendering.OpenGL.GL.Polygons,
+   module Graphics.Rendering.OpenGL.GL.PixelRectangles,
+   module Graphics.Rendering.OpenGL.GL.Fog,
+
+   -- * Per-Fragment Operations and the Framebuffer
+   module Graphics.Rendering.OpenGL.GL.PerFragment,
+   module Graphics.Rendering.OpenGL.GL.Framebuffer,
+
+   -- * Special Functions
+   module Graphics.Rendering.OpenGL.GL.FlushFinish,
+   module Graphics.Rendering.OpenGL.GL.Hints,
+
+   -- * State and State Requests
+   module Graphics.Rendering.OpenGL.GL.StateVar,
+   module Graphics.Rendering.OpenGL.GL.StringQueries,
+
+   -- * Temporary Modules, will die soon...
+   module Graphics.Rendering.OpenGL.GL.PixelRect,
+   module Graphics.Rendering.OpenGL.GL.Texturing
 ) where
 
-import Graphics.Rendering.OpenGL.GL.Antialiasing
 import Graphics.Rendering.OpenGL.GL.BasicTypes
 import Graphics.Rendering.OpenGL.GL.BeginEnd
-import Graphics.Rendering.OpenGL.GL.Clipping
-import Graphics.Rendering.OpenGL.GL.Colors
-import Graphics.Rendering.OpenGL.GL.CoordTrans
-import Graphics.Rendering.OpenGL.GL.FlushFinish
-import Graphics.Rendering.OpenGL.GL.Fog
-import Graphics.Rendering.OpenGL.GL.Framebuffer
-import Graphics.Rendering.OpenGL.GL.Hints
-import Graphics.Rendering.OpenGL.GL.LineSegments
-import Graphics.Rendering.OpenGL.GL.PerFragment
-import Graphics.Rendering.OpenGL.GL.PixelRect
-import Graphics.Rendering.OpenGL.GL.PixelRectangles
-import Graphics.Rendering.OpenGL.GL.Points
-import Graphics.Rendering.OpenGL.GL.Polygons
-import Graphics.Rendering.OpenGL.GL.RasterPos
+import Graphics.Rendering.OpenGL.GL.VertexSpec
+import Graphics.Rendering.OpenGL.GL.VertexArrays
 import Graphics.Rendering.OpenGL.GL.Rectangles
+import Graphics.Rendering.OpenGL.GL.CoordTrans
+import Graphics.Rendering.OpenGL.GL.Clipping
+import Graphics.Rendering.OpenGL.GL.RasterPos
+import Graphics.Rendering.OpenGL.GL.Colors
+
+import Graphics.Rendering.OpenGL.GL.Antialiasing
+import Graphics.Rendering.OpenGL.GL.Points
+import Graphics.Rendering.OpenGL.GL.LineSegments
+import Graphics.Rendering.OpenGL.GL.Polygons
+import Graphics.Rendering.OpenGL.GL.PixelRectangles
+import Graphics.Rendering.OpenGL.GL.Fog
+
+import Graphics.Rendering.OpenGL.GL.PerFragment
+import Graphics.Rendering.OpenGL.GL.Framebuffer
+
+import Graphics.Rendering.OpenGL.GL.FlushFinish
+import Graphics.Rendering.OpenGL.GL.Hints
+
 import Graphics.Rendering.OpenGL.GL.StateVar
 import Graphics.Rendering.OpenGL.GL.StringQueries
+
+import Graphics.Rendering.OpenGL.GL.PixelRect
 import Graphics.Rendering.OpenGL.GL.Texturing
-import Graphics.Rendering.OpenGL.GL.VertexArrays
-import Graphics.Rendering.OpenGL.GL.VertexSpec
