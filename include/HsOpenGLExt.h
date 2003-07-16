0028c8eac734f9f32118a5896579088609fe3355
panne <unknown>
2003-07-16 12:19:36 +0000

    [project @ 2003-07-16 12:19:36 by panne] The EXTENSION_ENTRY macro must immediately start with the foreign declaration, otherwise the magic mangler (hack_foreign) in the Hugs build system doesn't recognize it.

--- include/HsOpenGLExt.h
+++ include/HsOpenGLExt.h
@@ -15,10 +15,13 @@
 #ifndef HSOPENGLEXT_H
 #define HSOPENGLEXT_H
 
+/* NOTE: The macro must immediately start with the foreign declaration,
+   otherwise the magic mangler (hack_foreign) in the Hugs build system
+   doesn't recognize it. */
 #define EXTENSION_ENTRY(_msg,_entry,_ty) \
+foreign import CALLCONV unsafe "dynamic" dyn_/**/_entry :: Graphics.Rendering.OpenGL.GL.Extensions.Invoker (_ty) ; \
 _entry :: (_ty) ; \
 _entry = dyn_/**/_entry ptr_/**/_entry ; \
-foreign import CALLCONV unsafe "dynamic" dyn_/**/_entry :: Graphics.Rendering.OpenGL.GL.Extensions.Invoker (_ty) ; \
 ptr_/**/_entry :: FunPtr a ; \
 ptr_/**/_entry = unsafePerformIO (Graphics.Rendering.OpenGL.GL.Extensions.getProcAddress (_msg) ("_entry")) ; \
 {-# NOINLINE ptr_/**/_entry #-}
