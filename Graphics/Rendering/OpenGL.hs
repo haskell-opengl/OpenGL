6fe22bce8b7b5f89dfff37c40a880b01df22a202
panne <unknown>
2004-02-01 16:23:54 +0000

    [project @ 2004-02-01 16:23:54 by panne] Updated supported extensions

--- Graphics/Rendering/OpenGL.hs
+++ Graphics/Rendering/OpenGL.hs
@@ -41,8 +41,18 @@ import Graphics.Rendering.OpenGL.GLU
 -- GL_ARB_imaging                 |
 -- GL_ARB_multisample             | 1.3
 -- GL_ARB_multitexture            | 1.3
+-- GL_ARB_occlusion_query         | 1.5
 -- GL_ARB_point_parameters        | 1.4
 -- GL_ARB_point_sprite            |
+-- GL_ARB_shadow                  | 1.4
+-- GL_ARB_texture_border_clamp    | 1.3
+-- GL_ARB_texture_compression     | 1.3
+-- GL_ARB_texture_cube_map        | 1.3
+-- GL_ARB_texture_env_add         | 1.3
+-- GL_ARB_texture_env_combine     | 1.3
+-- GL_ARB_texture_env_crossbar    | 1.4
+-- GL_ARB_texture_env_dot3        | 1.3
+-- GL_ARB_texture_mirrored_repeat | 1.4
 -- GL_ARB_transpose_matrix        | 1.3
 -- GL_ARB_vertex_buffer_object    | 1.5
 -- GL_ARB_window_pos              | 1.4
@@ -50,19 +60,33 @@ import Graphics.Rendering.OpenGL.GLU
 -- GL_EXT_bgra                    | 1.2
 -- GL_EXT_blend_color             | 1.4
 -- GL_EXT_blend_func_separate     | 1.4
+-- GL_EXT_blend_logic_op          | 1.1
 -- GL_EXT_blend_minmax            | 1.4
 -- GL_EXT_blend_subtract          | 1.4
+-- GL_EXT_color_subtable          |
 -- GL_EXT_compiled_vertex_array   |
+-- GL_EXT_convolution             |
+-- GL_EXT_copy_texture            | 1.1
 -- GL_EXT_depth_bounds_test       |
 -- GL_EXT_draw_range_elements     | 1.2
 -- GL_EXT_fog_coord               | 1.4
+-- GL_EXT_histogram               |
 -- GL_EXT_multi_draw_arrays       | 1.4
 -- GL_EXT_packed_pixels           | 1.2
+-- GL_EXT_polygon_offset          | 1.1
 -- GL_EXT_rescale_normal          | 1.2
 -- GL_EXT_secondary_color         | 1.4
 -- GL_EXT_separate_specular_color | 1.2
+-- GL_EXT_shadow_funcs            | 1.5
 -- GL_EXT_stencil_two_side        |
 -- GL_EXT_stencil_wrap            | 1.4
+-- GL_EXT_subtexture              | 1.1
+-- GL_EXT_texture                 | 1.1
+-- GL_EXT_texture3D               | 1.2
+-- GL_EXT_texture_lod_bias        | 1.4
+-- GL_EXT_texture_object          | 1.1
+-- GL_EXT_vertex_array            | 1.1
+-- GL_HP_convolution_border_modes |
 -- GL_IBM_rasterpos_clip          |
 -- GL_NV_blend_square             | 1.4
 -- GL_NV_depth_clamp              |
@@ -70,6 +94,11 @@ import Graphics.Rendering.OpenGL.GLU
 -- GL_NV_light_max_exponent       |
 -- GL_NV_packed_depth_stencil     |
 -- GL_NV_primitive_restart        |
+-- GL_SGIS_generate_mipmap        | 1.4
+-- GL_SGIS_texture_edge_clamp     |
+-- GL_SGIS_texture_lod            |
+-- GL_SGI_color_matrix            |
+-- GL_SGI_color_table             |
 -- @
 
 --------------------------------------------------------------------------------
