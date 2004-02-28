5b4ba3f65d337d153b50027f3a859f967f18e32d
panne <unknown>
2004-02-28 18:45:37 +0000

    [project @ 2004-02-28 18:45:37 by panne] Moved ComparisonFunction into its own module

---
+++ Graphics/Rendering/OpenGL/GL/ComparisonFunction.hs
@@ -0,0 +1,57 @@
+-- #hide
+--------------------------------------------------------------------------------
+-- |
+-- Module      :  Graphics.Rendering.OpenGL.GL.ComparisonFunction
+-- Copyright   :  (c) Sven Panne 2002-2004
+-- License     :  BSD-style (see the file libraries/OpenGL/LICENSE)
+-- 
+-- Maintainer  :  sven.panne@aedion.de
+-- Stability   :  provisional
+-- Portability :  portable
+--
+-- This is a purely internal module for (un-)marshaling ComparisonFunction.
+--
+--------------------------------------------------------------------------------
+
+module Graphics.Rendering.OpenGL.GL.ComparisonFunction (
+   ComparisonFunction(..), marshalComparisonFunction,
+   unmarshalComparisonFunction
+) where
+
+import Graphics.Rendering.OpenGL.GL.BasicTypes ( GLenum )
+
+--------------------------------------------------------------------------------
+
+data ComparisonFunction =
+     Never
+   | Less
+   | Equal
+   | Lequal
+   | Greater
+   | Notequal
+   | Gequal
+   | Always
+   deriving ( Eq, Ord, Show )
+
+marshalComparisonFunction :: ComparisonFunction -> GLenum
+marshalComparisonFunction x = case x of
+   Never -> 0x200
+   Less -> 0x201
+   Equal -> 0x202
+   Lequal -> 0x203
+   Greater -> 0x204
+   Notequal -> 0x205
+   Gequal -> 0x206
+   Always -> 0x207
+
+unmarshalComparisonFunction :: GLenum -> ComparisonFunction
+unmarshalComparisonFunction x
+   | x == 0x200 = Never
+   | x == 0x201 = Less
+   | x == 0x202 = Equal
+   | x == 0x203 = Lequal
+   | x == 0x204 = Greater
+   | x == 0x205 = Notequal
+   | x == 0x206 = Gequal
+   | x == 0x207 = Always
+   | otherwise = error ("unmarshalComparisonFunction: illegal value " ++ show x)
--- Graphics/Rendering/OpenGL/GL/PerFragment.hs
+++ Graphics/Rendering/OpenGL/GL/PerFragment.hs
@@ -62,6 +62,8 @@ import Graphics.Rendering.OpenGL.GL.Capability (
    makeCapability, makeStateVarMaybe )
 import Graphics.Rendering.OpenGL.GL.BasicTypes (
    GLint, GLuint, GLsizei, GLenum, GLclampf, GLclampd, Capability )
+import Graphics.Rendering.OpenGL.GL.ComparisonFunction ( ComparisonFunction(..),
+   marshalComparisonFunction, unmarshalComparisonFunction )
 import Graphics.Rendering.OpenGL.GL.CoordTrans ( Position(..), Size(..) )
 import Graphics.Rendering.OpenGL.GL.Exception ( bracket_ )
 import Graphics.Rendering.OpenGL.GL.Extensions (
@@ -131,42 +133,6 @@ depthBounds =
       (uncurry glDepthBoundsEXT)
        
 EXTENSION_ENTRY("GL_EXT_depth_bounds_test",glDepthBoundsEXT,GLclampd -> GLclampd -> IO ())
-
---------------------------------------------------------------------------------
-
-data ComparisonFunction =
-     Never
-   | Less
-   | Equal
-   | Lequal
-   | Greater
-   | Notequal
-   | Gequal
-   | Always
-   deriving ( Eq, Ord, Show )
-
-marshalComparisonFunction :: ComparisonFunction -> GLenum
-marshalComparisonFunction x = case x of
-   Never -> 0x200
-   Less -> 0x201
-   Equal -> 0x202
-   Lequal -> 0x203
-   Greater -> 0x204
-   Notequal -> 0x205
-   Gequal -> 0x206
-   Always -> 0x207
-
-unmarshalComparisonFunction :: GLenum -> ComparisonFunction
-unmarshalComparisonFunction x
-   | x == 0x200 = Never
-   | x == 0x201 = Less
-   | x == 0x202 = Equal
-   | x == 0x203 = Lequal
-   | x == 0x204 = Greater
-   | x == 0x205 = Notequal
-   | x == 0x206 = Gequal
-   | x == 0x207 = Always
-   | otherwise = error ("unmarshalComparisonFunction: illegal value " ++ show x)
 
 --------------------------------------------------------------------------------
 
