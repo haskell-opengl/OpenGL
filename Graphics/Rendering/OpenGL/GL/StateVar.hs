d3e393edae9d99235cdd4357a909c96221d3ccc5
panne <unknown>
2004-03-19 22:35:11 +0000

    [project @ 2004-03-19 22:35:11 by panne] * Deprecated `set', it takes away a rather common name for something trivial. * Added HasGetter/HasSetter instances for IORef, this popped up several times. * Added a modificator convenience function, which is quite handy. * Added strict setters/modificators after some profiling of the examples.

--- Graphics/Rendering/OpenGL/GL/StateVar.hs
+++ Graphics/Rendering/OpenGL/GL/StateVar.hs
@@ -15,8 +15,11 @@ module Graphics.Rendering.OpenGL.GL.StateVar (
    GettableStateVar, makeGettableStateVar,
    HasSetter(..), set,
    SettableStateVar, makeSettableStateVar,
-   StateVar, makeStateVar
+   StateVar, makeStateVar,
+   ($~), ($=!), ($~!)
 ) where
+
+import Data.IORef ( IORef, readIORef, writeIORef )
 
 --------------------------------------------------------------------------------
 
@@ -42,6 +45,7 @@ makeGettableStateVar = GettableStateVar
 class HasSetter s where
    ($=) :: s a -> a -> IO ()
 
+{-# DEPRECATED set "use `sequence_' instead" #-}
 set :: [IO ()] -> IO ()
 set = sequence_
 
@@ -51,6 +55,10 @@ newtype SettableStateVar a = SettableStateVar (a -> IO ())
 
 instance HasSetter SettableStateVar where
    ($=) (SettableStateVar s) a = s a
+
+-- | A strict variant of '$='.
+($=!) :: HasSetter s => s a -> a -> IO ()
+v $=! x = x `seq` v $= x
 
 makeSettableStateVar :: (a -> IO ()) -> SettableStateVar a
 makeSettableStateVar = SettableStateVar
@@ -68,3 +76,26 @@ instance HasSetter StateVar where
 
 makeStateVar :: IO a -> (a -> IO ()) -> StateVar a
 makeStateVar g s = StateVar (makeGettableStateVar g) (makeSettableStateVar s)
+
+--------------------------------------------------------------------------------
+
+-- | A modificator convenience function.
+
+($~) :: (HasGetter v, HasSetter v) => v a -> (a -> a) -> IO ()
+v $~ f = do
+   x <- get v
+   v $= f x
+
+-- | A strict variant of '$~'.
+($~!) :: (HasGetter v, HasSetter v) => v a -> (a -> a) -> IO ()
+v $~! f = do
+   x <- get v
+   v $=! f x
+
+--------------------------------------------------------------------------------
+
+instance HasGetter IORef where
+   get = readIORef
+
+instance HasSetter IORef where
+   ($=) = writeIORef
