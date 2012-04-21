6166f3c582d0e19c652ae96ab0ce3c429e969b3c
Lars Corbijn <aspergesoepje@gmail.com>
2012-04-21 13:38:33 +0200

    Puts (unsafe) VertexAttrib queries in a module.

--- Graphics/Rendering/OpenGL/GL/QueryUtils.hs
+++ Graphics/Rendering/OpenGL/GL/QueryUtils.hs
@@ -15,21 +15,15 @@
 
 module Graphics.Rendering.OpenGL.GL.QueryUtils (
    module Graphics.Rendering.OpenGL.GL.QueryUtils.PName,
+   module Graphics.Rendering.OpenGL.GL.QueryUtils.VertexAttrib,
 
    lightIndexToEnum,
    modelviewIndexToEnum, modelviewEnumToIndex,
 
    maybeNullPtr,
-   AttribLocation(..), GetVertexAttribPName(..),
-   getVertexAttribInteger1, getVertexAttribEnum1, getVertexAttribBoolean1,
-   getVertexAttribFloat4, getVertexAttribIInteger4, getVertexAttribIuInteger4,
-   GetVertexAttribPointerPName(..), getVertexAttribPointer
 ) where
 
-import Foreign.Marshal.Alloc
 import Foreign.Ptr
-import Foreign.Storable
-import Graphics.Rendering.OpenGL.GL.PeekPoke
 import Graphics.Rendering.OpenGL.Raw.ARB.Compatibility (
    gl_LIGHT0, gl_MODELVIEW)
 import Graphics.Rendering.OpenGL.Raw.ARB.VertexBlend (
@@ -37,6 +31,7 @@ import Graphics.Rendering.OpenGL.Raw.ARB.VertexBlend (
 import Graphics.Rendering.OpenGL.Raw.Core32
 
 import Graphics.Rendering.OpenGL.GL.QueryUtils.PName
+import Graphics.Rendering.OpenGL.GL.QueryUtils.VertexAttrib
 
 --------------------------------------------------------------------------------
 
@@ -338,73 +333,3 @@ maybeNullPtr :: b -> (Ptr a -> b) -> Ptr a -> b
 maybeNullPtr n f ptr | ptr == nullPtr = n
                      | otherwise      = f ptr
 --------------------------------------------------------------------------------
-
-newtype AttribLocation = AttribLocation GLuint
-   deriving ( Eq, Ord, Show )
-
---------------------------------------------------------------------------------
-
-data GetVertexAttribPName =
-     GetVertexAttribArrayEnabled
-   | GetVertexAttribArraySize
-   | GetVertexAttribArrayStride
-   | GetVertexAttribArrayType
-   | GetVertexAttribArrayNormalized
-   | GetCurrentVertexAttrib
-   | GetVertexAttribArrayBufferBinding
-   | GetVertexAttribArrayInteger
-
-marshalGetVertexAttribPName :: GetVertexAttribPName -> GLenum
-marshalGetVertexAttribPName x = case x of
-   GetVertexAttribArrayEnabled -> gl_VERTEX_ATTRIB_ARRAY_ENABLED
-   GetVertexAttribArraySize -> gl_VERTEX_ATTRIB_ARRAY_SIZE
-   GetVertexAttribArrayStride -> gl_VERTEX_ATTRIB_ARRAY_STRIDE
-   GetVertexAttribArrayType -> gl_VERTEX_ATTRIB_ARRAY_TYPE
-   GetVertexAttribArrayNormalized -> gl_VERTEX_ATTRIB_ARRAY_NORMALIZED
-   GetCurrentVertexAttrib -> gl_CURRENT_VERTEX_ATTRIB
-   GetVertexAttribArrayBufferBinding -> gl_VERTEX_ATTRIB_ARRAY_BUFFER_BINDING
-   GetVertexAttribArrayInteger -> gl_VERTEX_ATTRIB_ARRAY_INTEGER
-
---------------------------------------------------------------------------------
-
-getVertexAttribInteger1 :: (GLint -> b) -> AttribLocation -> GetVertexAttribPName -> IO b
-getVertexAttribInteger1 f (AttribLocation location) n = alloca $ \buf -> do
-   glGetVertexAttribiv location (marshalGetVertexAttribPName n) buf
-   peek1 f buf
-
-getVertexAttribEnum1 :: (GLenum -> b) -> AttribLocation -> GetVertexAttribPName -> IO b
-getVertexAttribEnum1 f = getVertexAttribInteger1 (f . fromIntegral)
-
-getVertexAttribBoolean1 :: (GLboolean -> b) -> AttribLocation -> GetVertexAttribPName -> IO b
-getVertexAttribBoolean1 f = getVertexAttribInteger1 (f . fromIntegral)
-
-getVertexAttribFloat4 :: (GLfloat -> GLfloat -> GLfloat -> GLfloat -> b) -> AttribLocation -> GetVertexAttribPName -> IO b
-getVertexAttribFloat4 f (AttribLocation location) n = alloca $ \buf -> do
-   glGetVertexAttribfv location (marshalGetVertexAttribPName n) buf
-   peek4 f buf
-
-getVertexAttribIInteger4 :: (GLint -> GLint -> GLint -> GLint -> b) -> AttribLocation -> GetVertexAttribPName -> IO b
-getVertexAttribIInteger4 f (AttribLocation location) n = alloca $ \buf -> do
-   glGetVertexAttribIiv location (marshalGetVertexAttribPName n) buf
-   peek4 f buf
-
-getVertexAttribIuInteger4 :: (GLuint -> GLuint -> GLuint -> GLuint -> b) -> AttribLocation -> GetVertexAttribPName -> IO b
-getVertexAttribIuInteger4 f (AttribLocation location) n = alloca $ \buf -> do
-   glGetVertexAttribIuiv location (marshalGetVertexAttribPName n) buf
-   peek4 f buf
-
---------------------------------------------------------------------------------
-
-data GetVertexAttribPointerPName =
-   VertexAttribArrayPointer
-
-marshalGetVertexAttribPointerPName :: GetVertexAttribPointerPName -> GLenum
-marshalGetVertexAttribPointerPName x = case x of
-   VertexAttribArrayPointer -> gl_VERTEX_ATTRIB_ARRAY_POINTER
-
---------------------------------------------------------------------------------
-
-getVertexAttribPointer :: AttribLocation -> GetVertexAttribPointerPName -> IO (Ptr a)
-getVertexAttribPointer (AttribLocation location) n = alloca $ \buf -> do
-   glGetVertexAttribPointerv location (marshalGetVertexAttribPointerPName n) buf
-   peek buf
