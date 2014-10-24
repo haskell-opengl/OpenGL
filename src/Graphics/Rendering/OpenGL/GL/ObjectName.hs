950e51cf21b2d286f4ab84176da00c918ac5f6c1
Sven Panne <svenpanne@gmail.com>
2014-10-24 14:20:39 +0200

    Improved ObjectName documentation a bit.

--- src/Graphics/Rendering/OpenGL/GL/ObjectName.hs
+++ src/Graphics/Rendering/OpenGL/GL/ObjectName.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP #-}
 --------------------------------------------------------------------------------
 -- |
 -- Module      :  Graphics.Rendering.OpenGL.GL.ObjectName
@@ -12,7 +13,9 @@
 -- OpenGL 4.4 specs.
 --
 -- Object names are explicitly handled identifiers for API objects, e.g. a
--- texture object name in OpenGL or a buffer object name in OpenAL.
+-- texture object name in OpenGL or a buffer object name in OpenAL. They come in
+-- two flavors: If a name can exists on its own without an associated object, we
+-- have a 'GeneratableObjectName', otherwise we have an 'ObjectName'.
 --
 --------------------------------------------------------------------------------
 
@@ -20,23 +23,28 @@ module Graphics.Rendering.OpenGL.GL.ObjectName (
    ObjectName(..), GeneratableObjectName(..)
 ) where
 
-import Control.Monad
+import Control.Monad ( replicateM )
 
 --------------------------------------------------------------------------------
 
 -- | An 'ObjectName' is an explicitly handled identifier for API objects, e.g. a
 -- texture object name in OpenGL or a buffer object name in OpenAL.
+#if __GLASGOW_HASKELL__ < 708
 --
 -- Minimal complete definition: 'isObjectName' plus one of 'deleteObjectName' or
 -- 'deleteObjectNames'.
+#endif
 
 class ObjectName a where
+#if __GLASGOW_HASKELL__ >= 708
+   {-# MINIMAL isObjectName, ( deleteObjectName | deleteObjectNames ) #-}
+#endif
    -- | Test if the given object name is currently in use, i.e. test if it has
    -- been generated, but not been deleted so far.
    isObjectName :: a -> IO Bool
 
    -- | Make the given object name available again, declaring it as unused.
-   deleteObjectName:: a -> IO ()
+   deleteObjectName :: a -> IO ()
    deleteObjectName = deleteObjectNames . (:[])
 
    -- | Bulk version of 'deleteObjectName'.
@@ -47,10 +55,15 @@ class ObjectName a where
 -- creating an associated object at the same time, e.g. a buffer object name.
 -- Note that e.g. program object names do not fall into this category, because
 -- you can only create such a name together with a program object itself.
+#if __GLASGOW_HASKELL__ < 708
 --
 -- Minimal complete definition: One of 'genObjectName' or 'genObjectNames'.
+#endif
 
 class ObjectName a => GeneratableObjectName  a where
+#if __GLASGOW_HASKELL__ >= 708
+   {-# MINIMAL genObjectName | genObjectNames #-}
+#endif
    -- | Generate a new unused object name. By generating the name, it becomes
    -- used.
    genObjectName :: IO a
