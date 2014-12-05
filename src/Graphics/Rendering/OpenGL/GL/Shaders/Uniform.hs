c817f53c754b7f5f6471fc43377449028c89205b
Sven Panne <svenpanne@gmail.com>
2014-12-05 13:05:22 +0100

    Fixed indentation.

--- src/Graphics/Rendering/OpenGL/GL/Shaders/Uniform.hs
+++ src/Graphics/Rendering/OpenGL/GL/Shaders/Uniform.hs
@@ -139,11 +139,10 @@ makeUniformVar :: (UniformComponent a, Storable (b a))
                => (UniformLocation -> b a -> IO ())
                -> UniformLocation -> StateVar (b a)
 makeUniformVar setter location = makeStateVar getter (setter location)
-   where getter = do
-            program <- fmap fromJust $ get currentProgram
-            allocaBytes maxUniformBufferSize  $ \buf -> do
-            getUniform program location buf
-            peek buf
+   where getter = do program <- fmap fromJust $ get currentProgram
+                     allocaBytes maxUniformBufferSize $ \buf -> do
+                        getUniform program location buf
+                        peek buf
 
 instance UniformComponent a => Uniform (Vertex2 a) where
    uniform = makeUniformVar $ \location (Vertex2 x y) -> uniform2 location x y
@@ -199,12 +198,11 @@ instance UniformComponent a => Uniform (Index1 a) where
 instance Uniform TextureUnit where
     uniform loc@(UniformLocation ul)  = makeStateVar getter setter
         where setter (TextureUnit tu) = uniform1 loc (fromIntegral tu :: GLint)
-              getter = do
-                 program <- fmap fromJust $ get currentProgram
-                 allocaBytes (sizeOf (undefined :: GLint))  $ \buf -> do
-                 glGetUniformiv (programID program) ul buf
-                 tuID <- peek buf
-                 return . TextureUnit $ fromIntegral tuID
+              getter = do program <- fmap fromJust $ get currentProgram
+                          allocaBytes (sizeOf (undefined :: GLint))  $ \buf -> do
+                             glGetUniformiv (programID program) ul buf
+                             tuID <- peek buf
+                             return . TextureUnit $ fromIntegral tuID
     uniformv location count = uniform1v location count . (castPtr :: Ptr TextureUnit -> Ptr GLint)
 
 --------------------------------------------------------------------------------
